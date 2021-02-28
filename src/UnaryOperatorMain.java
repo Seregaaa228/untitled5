881279f923001669d319f3e6931973a9af51d72c
Seregaaa228 <75165284+Seregaaa228@users.noreply.github.com>
2021-02-28 15:56:42 +0200

    homework

---
+++ src/UnaryOperatorMain.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.UnaryOperator;
+public class UnaryOperatorMain {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введите слово");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        UnaryOperator<String> unaryOperator = s -> {
+            StringBuilder sb=new StringBuilder();
+            for(int i=0; i<s.length(); i++)
+                sb.append(s.charAt(i)).append(s.charAt(i));
+            return sb.toString();
+        };
+        SetLettersDuplicate(string, unaryOperator);
+    }
+    static void SetLettersDuplicate(String s, UnaryOperator<String> unaryOperator){
+        System.out.println(unaryOperator.apply(s));
+    }
+}
+
+
